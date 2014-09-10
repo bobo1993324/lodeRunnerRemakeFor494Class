0f4ce77ec2f18a7465efc77b7238a9df3e636007
Boren Zhang <bobo1993324@gmail.com>
2014-09-11 04:21:28 +0800

    design maps in tilededitor

---
+++ Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateMap : MonoBehaviour {
+	public string mapName;
+	// Use this for initialization
+	void Start () {
+		loadMap ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void loadMap() {
+	
+	}
+}
