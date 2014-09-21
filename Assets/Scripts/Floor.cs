f70078b96e9a735716c2c103bcf2ad12a3fad81d
Boren Zhang <bobo1993324@gmail.com>
2014-09-22 07:35:29 +0800

    dig takes time

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class Floor : MonoBehaviour {
 	public float waitTime = 5f;
 	public float healTime = 2f;
+	public int digHPMax = 2;
+	public int digHP;
+	private bool dugByPlayerInLastSecond = false;
+	private bool coroutineStarted = false;
 	enum DigState {
 		NORMAL,
 		DIGGING,
@@ -12,6 +16,35 @@ public class Floor : MonoBehaviour {
 	}
 	DigState digState = DigState.NORMAL;
 	public void dig() {
+		dugByPlayerInLastSecond = true;
+		if (!coroutineStarted) {
+			coroutineStarted = true;
+			digHP = digHPMax;
+			digHP --;
+			StartCoroutine("digging");
+		}
+	}
+	IEnumerator digging() {
+		while (digHP > 0) {
+			dugByPlayerInLastSecond = false;
+			yield return new WaitForSeconds (1);
+			Debug.Log ("dig " + dugByPlayerInLastSecond + digHP); 
+			if (dugByPlayerInLastSecond == true) {
+				digHP --;
+			} else {
+				digHP ++;
+			}
+			if (digHP >= digHPMax) {
+				digState = DigState.NORMAL;
+				coroutineStarted = false;
+				return false;
+			}
+		}
+		digComplete ();
+		coroutineStarted = false;
+	}
+	private void digComplete() {
+		Debug.Log ("dig complete");
 		StopCoroutine ("waitForHealing");
 		digState = DigState.DUG;
 		gameObject.renderer.enabled = false;
