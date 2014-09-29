d9803f45e2278b705237c6f7cf9767612fbaeef5
Elliott <elliott8533@gmail.com>
2014-09-29 05:12:30 -0400

    Canera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,14 +4,37 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour {
 	GameObject runner;
 	// Update is called once per frame
+	public float XRightRange;
+	public float XLeftRange;
+	public float YTopRange;
+	public float YBottonRange;
+	public float StartX;
+	public float StartY;
+	public float CameraFollowDistance;
+	void Start() {
+		Vector3 pos = new Vector3 (StartX, StartY, transform.position.z);
+		transform.position = pos;
+	}
+
 	void Update() {
 		if (runner == null) {
 			runner = GetComponent<GenerateMap>().getPlayer();
 		}
 		if (runner != null) {
 			Vector3 pos = transform.position;
-			pos.x = runner.transform.position.x;
-			pos.y = runner.transform.position.y;
+			Vector3 runnerPos = runner.transform.position;
+			if ( runnerPos.x <= XRightRange && runnerPos.x >= XLeftRange ){
+				if (Mathf.Abs(pos.x - runnerPos.x) >= CameraFollowDistance) {
+					if (runnerPos.x > pos.x) {
+						pos.x = runnerPos.x - CameraFollowDistance;
+					}else if (runnerPos.x < pos.x){
+						pos.x = runnerPos.x + CameraFollowDistance;
+					}
+				}
+			}
+			if ( runnerPos.y <= YTopRange && runnerPos.y >= YBottonRange ){
+				pos.y = runnerPos.y;
+			}
 			transform.position = pos;
 		}
 	}
